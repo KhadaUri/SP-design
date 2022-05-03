63e643db6c27c44770b58ba9f224a1091630360c
iure <idknameuri@outlook.com>
2022-05-02 22:37:13 -0500

    SP

---
+++ index.php
@@ -0,0 +1,390 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<title>J&amp;O Seguridad Privada</title>
+<meta content="Somos una empresa 100% mexicana especializada en ofrecer soluciones integrales en materia de seguridad, enfocados en el servicio y profesionalismo con el fin de satisfacer las necesidades de nuestros clientes" name="description">
+<meta content="" name="keywords">
+
+<link rel="shortcut icon" href="https://www.jyoseguridadprivada.com/assets/img/favicon.png">
+<link rel="icon" type="image/png" sizes="192x192" href="https://www.jyoseguridadprivada.com/assets/img/favicon192.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://www.jyoseguridadprivada.com/assets/img/favicon180.png">
+
+<link href="assets/css.css" rel="stylesheet">
+<link rel="stylesheet" href="assets/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+<link href="assets/bootstrap.css" rel="stylesheet">
+<link href="assets/owl.css" rel="stylesheet">
+<link href="assets/venobox.css" rel="stylesheet">
+<link href="assets/aos.css" rel="stylesheet">
+
+<link href="assets/style.css" rel="stylesheet">
+<style>[_nghost-wef-c63]{font-family:Open Sans,sans-serif;color:#121212}</style></head>
+<body data-aos-easing="ease" data-aos-duration="1000" data-aos-delay="0"><button type="button" class="mobile-nav-toggle d-lg-none"><i class="fas fa-bars"></i></button>
+
+<header id="header" class="fixed-top">
+<div class="container d-flex align-items-center justify-content-between">
+<h1 class="logo"><a href="https://www.jyoseguridadprivada.com/">J&amp;O<span>Seguridad Privada</span></a></h1>
+
+
+<nav class="nav-menu d-none d-lg-block">
+<ul>
+<li class="active"><a href="https://www.jyoseguridadprivada.com/">Inicio</a></li>
+<li class=""><a href="#about">Nosotros</a></li>
+<li class=""><a href="#services">Servicios</a></li>
+<li><a href="#portfolio">Portafolio</a></li>
+<li><a href="#team">Equipo</a></li>
+<li class=""><a href="#contact" class="get-started-btn scrollto">Contacto</a></li>
+</ul>
+</nav>
+</div>
+</header>
+
+<section id="hero" class="d-flex align-items-center justify-content-center">
+<div class="container">
+<div class="row">
+<div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+<h1 data-aos="fade-up" class="aos-init aos-animate">J&amp;O Seguridad privada S.A. DE C.V.</h1>
+<h2 class="mb-3 aos-init aos-animate" data-aos="fade-up" data-aos-delay="400">Es una empresa 100% Mexicana con más de 15 años de experiencia en el mercado nacional de la Seguridad Privada.</h2>
+<div data-aos="fade-up my-3" data-aos-delay="800" class="aos-init aos-animate">
+<a href="#about" class="btn-get-started scrollto">Conocer más</a>
+</div>
+</div>
+<div class="col-lg-6 order-1 order-lg-2 hero-img aos-init aos-animate" data-aos="fade-left" data-aos-delay="200">
+<img src="assets/JOpng.png" class="img-fluid animated" alt="logo">
+</div>
+</div>
+</div>
+</section>
+<main id="main">
+
+<section id="about" class="about">
+<div class="container aos-init aos-animate" data-aos="fade-up">
+<div class="row">
+<div class="col-lg-6 order-1 order-lg-2 aos-init aos-animate" data-aos="fade-left" data-aos-delay="100">
+<img src="assets/about.jpg" class="img-fluid" alt="about">
+</div>
+<div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content aos-init aos-animate" data-aos="fade-right" data-aos-delay="100">
+<h3>Somos una empresa 100% mexicana.</h3>
+<p class="font-italic">
+Especializada en ofrecer
+soluciones integrales en materia de seguridad, enfocados en
+el servicio y profesionalismo con el fin de satisfacer las
+necesidades de nuestros clientes en las areas:
+</p>
+<ul>
+<li><i class="ri-check-double-line"></i> Corporativas.</li>
+<li><i class="ri-check-double-line"></i> Residenciales.</li>
+<li><i class="ri-check-double-line"></i> Industriales.</li>
+<li><i class="ri-check-double-line"></i> Centros comerciales.</li>
+</ul>
+<p>
+Nos caracterizamos por entregar los más altos estándares de
+calidad en los servicios y nuestra capacidad de desarrollar
+soluciones personalizadas ligada a una estrategia de seguridad
+integral.
+</p>
+</div>
+</div>
+</div>
+</section>
+
+<section id="counts" class="counts">
+<div class="container aos-init aos-animate" data-aos="fade-up">
+<div class="row no-gutters">
+<div class="image col-xl-5 d-flex align-items-stretch justify-content-center justify-content-lg-start aos-init aos-animate" data-aos="fade-right" data-aos-delay="100"></div>
+<div class="col-xl-7 pl-0 pl-lg-5 pr-lg-1 d-flex align-items-stretch aos-init aos-animate" data-aos="fade-left" data-aos-delay="100">
+<div class="content d-flex flex-column justify-content-center">
+<h3>ELEMENTOS ALTAMENTE CAPACITADOS Y EQUIPADOS</h3>
+<p>Nos especializamos en el análisis de riesgo y la
+implementación de los sistemas de seguridad y vigilancia que
+minimicen las vulnerabilidades inherentes a la actividad y
+ubicación geográfica de los clientes
+</p>
+<div class="row my-3">
+<div class="col-md-6 d-md-flex align-items-md-stretch">
+<div class="count-box">
+<i class="icofont-simple-smile"></i>
+<span data-toggle="counter-up">165</span>
+<p><strong>Clientes</strong> Nos han brindado su confianza.</p>
+</div>
+</div>
+<div class="col-md-6 d-md-flex align-items-md-stretch">
+<div class="count-box">
+<i class="icofont-clock-time"></i>
+<span data-toggle="counter-up">15</span>
+<p><strong>Años de experiencia</strong> en el mercado nacional de la Seguridad Privada.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section id="client_c" class="client_c">
+<div class="container aos-init aos-animate" data-aos="zoom-in">
+<div class="text-center">
+<h3> Nuestra empresa se encarga de promover y establecer los medios 
+necesarios, para que la comunicación con nuestros clientes, sea de una 
+forma proactiva, eficaz y eficiente, para resolver sus necesidades.</h3>
+</div>
+</div>
+</section>
+<section id="features" class="features py-5 my-5">
+<div class="container aos-init aos-animate" data-aos="fade-up">
+<div class="row">
+<div class="image col-lg-6 aos-init aos-animate" style="background-image: url(&quot;assets/img/features.jpg&quot;);" data-aos="fade-right"></div>
+<div class="col-lg-6 aos-init aos-animate" data-aos="fade-left" data-aos-delay="100">
+<div class="section-title">
+<h2>Areas de Trabajo</h2>
+<p>J&amp;O Seguridad Privada</p>
+</div>
+<div class="icon-box mt-5 mt-lg-0 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="150">
+<i class="fas fa-city p-3"></i>
+<h4>Corporativos</h4>
+<p class="">Garantizar la efectiva protección de las personas, de los 
+activos físicos y lógicos y de la información, así como de la privacidad
+ de los datos tratados.</p>
+</div>
+<div class="icon-box mt-5 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="150">
+<i class="fas fa-home p-3"></i>
+<h4>Residenciales</h4>
+<p>Cualidad de la vivienda, entorno y conjunto que permite el desarrollo
+ de actividades en condiciones de tranquilidad y exención de peligro.</p>
+</div>
+<div class="icon-box mt-5 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="150">
+<i class="fas fa-industry p-3"></i>
+<h4>Industriales</h4>
+<p>Ejecutamos exitosos proyectos de seguridad privada industrial, tanto en fábricas como en plantas o parques industriales.</p>
+</div>
+<div class="icon-box mt-5 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="150">
+<i class="fas fa-shopping-cart p-3"></i>
+<h4>Centros comerciales</h4>
+<p>Contemplamos no sólo los riesgos de origen técnico como los posibles 
+incendios, cortes de suministro o fenómenos naturales, sino también los 
+riesgos de origen humano: hurtos, robos, vandalismo, aglomeraciones, 
+amenazas de atentado, etc.</p>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<section id="cta" class="cta">
+<div class="container aos-init aos-animate" data-aos="zoom-in">
+<div class="text-center">
+<h3>VISIÓN</h3>
+<p>Ser una empresa líder en seguridad privada, por medio del mejor 
+capital humano, tecnológico y logístico disponible para garantizar los 
+mejores servicios a nuestros clientes.
+</p>
+<h3>MISIÓN</h3>
+<p>Brindar los mejores servicios de seguridad privada a cada cliente, 
+dando asesoría legal y llevando acabo investigaciones con los más altos 
+estándares de calidad.</p>
+</div>
+</div>
+</section>
+
+<section id="services" class="services">
+<div class="container aos-init aos-animate" data-aos="fade-up">
+<div class="section-title">
+<h2>Servicios</h2>
+<p>J&amp;O Seguridad Privada</p>
+</div>
+<div class="row">
+<div class="col-lg-4 col-md-6 d-flex align-items-stretch aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+<div class="icon-box">
+<div class="icon"><i class="fas fa-user-shield p-3"></i></div>
+<h4><a href="">Guardias de seguridad</a></h4>
+<p>En nuestro servicio de protección con guardias, desarrollamos
+soluciones de seguridad rentables que se adaptan al entorno y
+necesidades de nuestros clientes, ofreciendo una amplia gama
+de opciones a elegir</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="200">
+<div class="icon-box">
+<div class="icon"><i class="fas fa-video p-3"></i></div>
+<h4><a href="">Video Vigilancia</a></h4>
+<p>Área especializada para el desarrollo de soluciones integrales de video vigilancia, la cual se
+encarga del suministro e instalación de sistemas avanzados de alta tecnología en cámaras,
+grabadores digitales, software de administración y gestión, analíticos, equipos de protección
+y respaldo, etc</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="300">
+<div class="icon-box">
+<div class="icon"><i class="fas fa-id-badge p-3"></i></div>
+<h4><a href="">Control de acceso y trafico vehicular</a></h4>
+<p>Nuestras soluciones de control de acceso vehicular, peatonal y de
+personal están diseñadas para controlar y supervisar los accesos que se
+realizan en las diferentes zonas del lugar que pretendemos asegurar,
+permitiendo gestionar la seguridad de las instalaciones y mantener un
+control de flujo de la gente/vehículos</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+<div class="icon-box">
+<div class="icon"><i class="fas fa-car p-3"></i></div>
+<h4><a href="">Custodia de Bienes</a></h4>
+<p>La custodia de bienes o custodia de mercancías consiste en todos los 
+recursos que se pueden emplear para brindar protección y seguridad para 
+el traslado de mercancías. </p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="200">
+<div class="icon-box">
+<div class="icon"><i class="fas fa-dungeon p-3"></i></div>
+<h4><a href="">Proteccion Perimetral</a></h4>
+<p>Contamos con una línea de productos que son la solución para
+alcanzar el nivel de protección idóneo para activos críticos, sin
+limitar la operación ni modificar necesariamente el concepto
+arquitectónico o el entorno.</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 aos-init aos-animate" data-aos="zoom-in" data-aos-delay="300">
+<div class="icon-box">
+<div class="icon"><i class="fas fa-clock p-3"></i></div>
+<h4><a href="">Asesoría legal las 24 HRS</a></h4>
+<p>Contamos con una gama alta de abogados especializados en materia, civil, familiar, penal, administrativo, corporativo, agrario. </p>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+
+
+<section id="contact" class="contact">
+<div class="container aos-init aos-animate" data-aos="fade-up">
+<div class="section-title">
+<h2>Contacto</h2>
+<p>Estamos a tus ordenes</p>
+</div>
+<div>
+<iframe style="border:0; width: 100%; height: 270px;" src="assets/embed.htm" allowfullscreen="" loading="lazy"></iframe>
+</div>
+<div class="row mt-5">
+<div class="col-lg-4">
+<div class="info">
+<div class="address">
+<i class="fas fa-map-marker-alt"></i>
+<h4>Dirección:</h4>
+<p>Av. Niños Heroes de Chapultepec 175, Miguel Alemán, Benito Juárez, 03440 Ciudad de México, CDMX </p>
+</div>
+<div class="email">
+<i class="fas fa-envelope"></i>
+<h4>Email:</h4>
+<p>contacto@jyoseguridadprivada.com</p>
+</div>
+<div class="phone">
+<i class="fas fa-phone"></i>
+<h4>Teléfonos:</h4>
+<p>+52 55 2609 9369</p>
+<p>+52 56 1265 4907</p>
+</div>
+</div>
+</div>
+<div class="col-lg-8 mt-5 mt-lg-0">
+<form id="form_contacto" class="php-email-form">
+<div class="form-row">
+<div class="col-md-6 form-group">
+<input type="text" name="name" class="form-control" id="name" placeholder="Nombre" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+<div class="validate"></div>
+</div>
+ <div class="col-md-6 form-group">
+<input type="email" class="form-control" name="email" id="email" placeholder="Email" data-rule="email" data-msg="Please enter a valid email">
+<div class="validate"></div>
+</div>
+</div>
+<div class="form-group">
+<input type="tel" class="form-control" name="subject" id="subject" placeholder="Teléfono" data-rule="minlen:4">
+<div class="validate"></div>
+</div>
+<div class="form-group">
+<textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Mensaje"></textarea>
+<div class="validate"></div>
+</div>
+<div class="text-center"><button type="submit">Enviar</button></div>
+</form>
+</div>
+</div>
+</div>
+</section>
+</main>
+
+<footer id="footer">
+<div class="footer-top">
+<div class="container">
+<div class="row">
+<div class="col-lg-3 col-md-6">
+<div class="footer-info">
+<h3 class="logo text-white"><a href="https://www.jyoseguridadprivada.com/">J&amp;O<span class=" text-white">Seguridad Privada</span></a></h3>
+<p>
+Av. Niños Heroes de Chapultepec 175, Miguel Alemán, Benito Juárez, 03440 Ciudad de México, CDMX<br><br>
+<strong>Teléfono:</strong> +52 55 2609 9369<br>
+<strong>Email:</strong> contacto@jyoseguridadprivada.com<br>
+</p>
+<div class="social-links mt-3">
+<a href="https://www.facebook.com/JO-Seguridad-Privada-104812375023096" rel="nofollow" class="facebook"><i class="fab fa-facebook-square"></i></a>
+</div>
+</div>
+</div>
+<div class="col-lg-2 col-md-6 footer-links">
+<h4>Links</h4>
+<ul>
+<li><i class="bx bx-chevron-right"></i> <a href="#">Inicio</a></li>
+<li><i class="bx bx-chevron-right"></i> <a href="#about">Nosotros</a></li>
+<li><i class="bx bx-chevron-right"></i> <a href="#services">Servicios</a></li>
+<li><i class="bx bx-chevron-right"></i> <a href="#">Politica de privacidad</a></li>
+</ul>
+</div>
+<div class="col-lg-3 col-md-6 footer-links">
+</div>
+<div class="col-lg-4 col-md-6 footer-newsletter">
+<h4>Subscribete a nuetro Newsletter</h4>
+<form action="" method="post">
+<input type="email" name="email"><input type="submit" value="Subscribete">
+</form>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="copyright">
+© Copyright <strong><span>J&amp;O Seguridad Privada</span></strong>. All Rights Reserved
+</div>
+
+</div>
+</footer>
+<a href="#" class="back-to-top" style="display: none;"><i class="fas fa-arrow-up"></i></a>
+
+
+<script src="assets/jquery_003.js"></script>
+<script src="assets/bootstrap.js"></script>
+<script src="assets/jquery_002.js"></script>
+<script src="assets/validate.js"></script>
+<script src="assets/owl.js"></script>
+<script src="assets/isotope.js"></script>
+<script src="assets/venobox.js"></script>
+<script src="assets/jquery.js"></script>
+<script src="assets/counterup.js"></script>
+<script src="assets/aos.js"></script>
+
+<script src="assets/main.js"></script><nav class="mobile-nav d-lg-none">
+<ul>
+<li class="active"><a href="https://www.jyoseguridadprivada.com/">Inicio</a></li>
+<li><a href="#about">Nosotros</a></li>
+<li><a href="#services">Servicios</a></li>
+<li><a href="#portfolio">Portafolio</a></li>
+<li><a href="#team">Equipo</a></li>
+<li><a href="#contact" class="get-started-btn scrollto">Contacto</a></li>
+</ul>
+</nav><div class="mobile-nav-overly"></div>
+
+
+</body><app-content ng-version="13.2.0"></app-content></html>
